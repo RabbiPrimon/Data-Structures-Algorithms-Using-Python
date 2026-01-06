116ae61bef6029484ed6acb757a39ea7a1bc20a4
Rabbi <rabbiprimon00000@gmail.com>
2026-01-06 20:18:07 +0600

    ..

--- Strings & Conditional Statements.py
+++ Strings & Conditional Statements.py
@@ -1,4 +1,21 @@
-## WAP to input users first name and print its length
+# ## WAP to input users first name and print its length
 
-name = input("Enter your name: ")
-print ("Length of your name is", len(name))
+# name = input("Enter your name: ")
+# print ("Length of your name is", len(name))
+
+
+#WAP to grade marks
+
+marks = int(input("Enter your marks : "))
+
+if (marks>=90):
+  grade = "A"
+
+elif (marks>= 80 and marks < 90):
+  grade = "B"
+elif (marks>=70 and marks <80):
+  grade = "C"
+else:
+  grade = "D"
+
+print("grade of the students :", grade)
