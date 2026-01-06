1826249d0892fd66279e21e1d8acbdcf1362f713
Rabbi <rabbiprimon00000@gmail.com>
2026-01-06 22:14:48 +0600

    New pblm added

--- Strings & Conditional Statements.py
+++ Strings & Conditional Statements.py
@@ -1,4 +1,4 @@
-## WAP to input users first name and print its length
+#WAP to input users first name and print its length
 
 name = input("Enter your name: ")
 print ("Length of your name is", len(name))
