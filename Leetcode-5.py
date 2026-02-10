651052b44022bb30d9802e7b826fd343ebf89705
Rabbi <rabbiprimon00000@gmail.com>
2026-02-10 21:03:33 +0600

    Leetcode-5 added

---
+++ Leetcode-5.py
@@ -0,0 +1,51 @@
+# 5. Longest Palindromic Substring
+# Medium
+# Topics
+# premium lock icon
+# Companies
+# Hint
+# Given a string s, return the longest palindromic substring in s.
+
+ 
+
+# Example 1:
+
+# Input: s = "babad"
+# Output: "bab"
+# Explanation: "aba" is also a valid answer.
+# Example 2:
+
+# Input: s = "cbbd"
+# Output: "bb"
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 1000
+# s consist of only digits and English letters.
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def expand(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
+
+        longest = ""
+
+        for i in range(len(s)):
+            # odd length palindrome
+            p1 = expand(i, i)
+
+            # even length palindrome
+            p2 = expand(i, i + 1)
+
+            if len(p1) > len(longest):
+                longest = p1
+
+            if len(p2) > len(longest):
+                longest = p2
+
+        return longest
